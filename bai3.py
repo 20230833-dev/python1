1c104c40ff9c9b4de06881822e894daf17528f50
20230833-dev <20230833@eaut.edu.vn>
2026-03-28 09:32:04 +0700

    26/3

---
+++ bai3.py
@@ -0,0 +1,11 @@
+a = int(input("nhap so nguyen thu nhat:"))
+b = int(input("nhap so nguyen thu hai:"))
+c = int(input("nhap so nguyen thu ba:"))
+tong_tich = a + b * c
+hieu = a - b 
+so_nguyen = a / b
+phan_du = a % b
+print(tong_tich)
+print(hieu)
+print(so_nguyen)
+print(phan_du)
---
+++ bai4.py
@@ -0,0 +1,6 @@
+Ho = input("ho:")
+ten_dem = input("ten dem:")
+ten = input("ten:")
+ketqua = Ho + ten_dem + ten
+uppercase_ketqua = ketqua.upper()
+print("Ket qua in ra la:", uppercase_ketqua)
