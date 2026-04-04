b9f010ab9f11578b84259b314b15e880924a9ffe
20230833-dev <20230833@eaut.edu.vn>
2026-04-04 09:35:51 +0700

    Update and rename bai2.py to bai2.py

--- chuong 4/bai2.py
+++
@@ -1,9 +0,0 @@
-text = input("Nhập đoạn văn: ")
-
-with open("output.txt", "w", encoding="utf-8") as f:
-    f.write(text)
-
-# Đọc lại để hiển thị
-with open("output.txt", "r", encoding="utf-8") as f:
-    print("Nội dung file:")
-    print(f.read())
